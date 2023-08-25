337c1126a417c05ba7a5c8fb080801157bddadd7
Akanksha Sahu <akankshasahu2299@gmail.com>
2023-08-25 23:22:54 +0530

    Add files via upload

---
+++ Includes/Footer.php
@@ -0,0 +1,63 @@
+<footer class="ftco-footer ftco-section">
+      <div class="container">
+        <div class="row d-flex">
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Your Perfect</h2>
+              <p>OUR SALON PROVIDES SERVICES TO MEN AND WOMEN OF ANY HAIR TYPE AND STYLE: EDGY OR CLASSIC, STRAIGHT OR CURLY, SHORT OR LONG.</p>
+              <ul class="ftco-footer-social list-unstyled float-lft mt-3">
+                <li class="ftco-animate"><a href="https://twitter.com" target="_blank"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="https://m.facebook.com" target="_blank"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="https://www.instagram.com" target="_blank"><span class="icon-instagram"></span></a></li>
+                <li class="ftco-animate"><a href="https://www.google.co.in/maps" target="_blank"><span class="icon-map"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2">Popular Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="#">Hair Treatment</a></li>
+                <li><a href="#">Nail Art</a></li>
+                <li><a href="#">Skin Care</a></li>
+                <li><a href="#">Body Massage</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Quick Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="about.php">About</a></li>
+                <li><a href="treatments.php">Services</a></li>
+                <li><a href="Specialists.php">Specialists</a></li>
+                <li><a href="Contact.php">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Have a Questions?</h2>
+              <div class="block-23 mb-3">
+                <ul>
+                  <li><span class="icon icon-map-marker"></span><span class="text">Shop No. 203 Elpro City Squre, Chinchwad, Pune</span></li>
+                  <li><a href="#"><span class="icon icon-phone"></span><span class="text">+91 9876543211</span></a></li>
+                  <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourperfect.com</span></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center">
+
+            <p class="mb-0">
+                            Copyright &copy;<script>
+                document.write(new Date().getFullYear());
+
+              </script> All rights reserved | This Website is made with <i class="icon-heart" aria-hidden="true"></i> by Megha And Akanksha</a>
+              </p>
+          </div>
+        </div>
+      </div>
+    </footer>
